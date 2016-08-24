52454cad732c929b0fe741edecc63a5186bb8c20
shlmk <kingofsherlockspades@gmail.com>
2016-08-23 20:04:24 -0400

    Begin creating the player functions

--- player_functions.R
+++ player_functions.R
@@ -1,7 +1,37 @@
+library(stringr)
+library(rjson)
+library(dplyr)
+
 #This function creates the URL that points to each player's data
 createPlayerURL <- function(id){
-  base <- "http://fantasy.premierleague.com/web/api/elements/"
+  base <- "https://fantasy.premierleague.com/drf/element-summary/"
+  return(str_c(base,id,sep=""))
+}
+
+getPlayerStats <- function(url){
+  jsonlite::fromJSON(url)
+}
+
+getAllPlayersInfo <- function(){
+  as.tbl(jsonlite::fromJSON("https://fantasy.premierleague.com/drf/elements/"))
+}
+
+getPlayerInfoWithName <- function(name){
+  players <- getAllPlayersInfo() %>% filter(web_name == name)
   
-  return(str_c(base,id,"/",sep=""))
-  
+  if(nrow(player) == 0){
+    stop("No player found")
+  }else{
+    return(player)
+  }
+} 
+
+getPlayerInfoWithID <- function(id){
+  players <- getAllPlayersInfo()
+
+  if(id < 1 || id > nrow(players)){
+    stop("Invalid index given")
+  }else{
+    return(players[id, ])
+  }
 }
